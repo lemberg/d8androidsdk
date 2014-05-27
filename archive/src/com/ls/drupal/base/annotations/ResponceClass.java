5bcd23a750dfaee51f198eba75eabc5768c29eaf
Taras Kunyk <taras.kunyk@lemberg.co.uk>
2014-05-27 11:42:35 +0300

    Odd files removed

--- archive/src/com/ls/drupal/base/annotations/ResponceClass.java
+++
@@ -1,6 +0,0 @@
-package com.ls.drupal.base.annotations;
-
-public @interface ResponceClass
-{
-	 Class<?> type();
-}
--- archive/src/com/ls/drupal/base/annotations/ResponceMapClass.java
+++
@@ -1,7 +0,0 @@
-package com.ls.drupal.base.annotations;
-
-public @interface ResponceMapClass
-{
-	 Class<?> keyType();
-	 Class<?> valueType();
-}
